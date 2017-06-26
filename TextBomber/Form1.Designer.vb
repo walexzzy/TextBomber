4f05d37928eb3497941220d61db9f7cacb3dd27d
GhostSyndic <ghostsyndic@protonmail.com>
2017-06-26 10:44:22 -0700

    First Commit

---
+++ TextBomber/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TextBomber
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPhoneNumber = New System.Windows.Forms.Label()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.txtMessage = New System.Windows.Forms.RichTextBox()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.ComboBoxCarrier = New System.Windows.Forms.ComboBox()
+        Me.lblCarrier = New System.Windows.Forms.Label()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblPhoneNumber
+        '
+        Me.lblPhoneNumber.AutoSize = True
+        Me.lblPhoneNumber.Location = New System.Drawing.Point(12, 103)
+        Me.lblPhoneNumber.Name = "lblPhoneNumber"
+        Me.lblPhoneNumber.Size = New System.Drawing.Size(78, 13)
+        Me.lblPhoneNumber.TabIndex = 0
+        Me.lblPhoneNumber.Text = "Phone Number"
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(97, 100)
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(168, 20)
+        Me.txtPhoneNumber.TabIndex = 1
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(97, 295)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(50, 25)
+        Me.btnSend.TabIndex = 2
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(316, 24)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Location = New System.Drawing.Point(97, 152)
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(168, 138)
+        Me.txtMessage.TabIndex = 6
+        Me.txtMessage.Text = ""
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Location = New System.Drawing.Point(12, 155)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(50, 13)
+        Me.lblMessage.TabIndex = 7
+        Me.lblMessage.Text = "Message"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(12, 36)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(74, 13)
+        Me.lblEmail.TabIndex = 8
+        Me.lblEmail.Text = "Gmail Address"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(12, 61)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(53, 13)
+        Me.lblPassword.TabIndex = 9
+        Me.lblPassword.Text = "Password"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(97, 36)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(168, 20)
+        Me.txtEmail.TabIndex = 10
+        '
+        'txtPassword
+        '
+        Me.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPassword.Location = New System.Drawing.Point(97, 62)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(168, 20)
+        Me.txtPassword.TabIndex = 11
+        Me.txtPassword.UseSystemPasswordChar = True
+        '
+        'ComboBoxCarrier
+        '
+        Me.ComboBoxCarrier.FormattingEnabled = True
+        Me.ComboBoxCarrier.Items.AddRange(New Object() {"AT&T", "Sprint", "T-Mobile", "Verizon"})
+        Me.ComboBoxCarrier.Location = New System.Drawing.Point(97, 127)
+        Me.ComboBoxCarrier.Name = "ComboBoxCarrier"
+        Me.ComboBoxCarrier.Size = New System.Drawing.Size(168, 21)
+        Me.ComboBoxCarrier.TabIndex = 12
+        '
+        'lblCarrier
+        '
+        Me.lblCarrier.AutoSize = True
+        Me.lblCarrier.Location = New System.Drawing.Point(15, 127)
+        Me.lblCarrier.Name = "lblCarrier"
+        Me.lblCarrier.Size = New System.Drawing.Size(37, 13)
+        Me.lblCarrier.TabIndex = 13
+        Me.lblCarrier.Text = "Carrier"
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(215, 297)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(50, 23)
+        Me.btnQuit.TabIndex = 14
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'TextBomber
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(316, 357)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.lblCarrier)
+        Me.Controls.Add(Me.ComboBoxCarrier)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtPhoneNumber)
+        Me.Controls.Add(Me.lblPhoneNumber)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "TextBomber"
+        Me.Text = "TextBomber"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblPhoneNumber As Label
+    Friend WithEvents txtPhoneNumber As TextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents txtMessage As RichTextBox
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents ComboBoxCarrier As ComboBox
+    Friend WithEvents lblCarrier As Label
+    Friend WithEvents btnQuit As Button
+End Class
---
+++ TextBomber/Form1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class TextBomber
+    Private Function IsPhoneNumberValid(ByVal Number As String) As Boolean
+
+        Dim PhoneValid As Boolean
+        Dim PhoneNumber As String = "^[1-9]\d{2}[1-9]\d{2}\d{4}$"
+        Dim ChekPhone As New Regex(PhoneNumber)
+        If Not String.IsNullOrEmpty(Number) Then
+
+            PhoneValid = ChekPhone.IsMatch(Number)
+        Else
+            PhoneValid = False
+
+        End If
+
+        Return PhoneValid
+    End Function
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+        If Not IsPhoneNumberValid(txtPhoneNumber.Text) Then
+            MessageBox.Show("Please enter a valid Phone Number!")
+            Return
+        End If
+
+
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Close()
+    End Sub
+End Class
